9c97ab2e44f7f54db7411e2afdac955255cbf7d0
ilyatim <ilyatimofeevgg@mail.ru>
2019-03-11 17:21:07 +0300

    Orientation Update

--- Design/Sipliy/app/src/main/java/com/example/sipliy/MainMenuActivity.java
+++ Design/Sipliy/app/src/main/java/com/example/sipliy/MainMenuActivity.java
@@ -1,6 +1,8 @@
 package com.example.sipliy;
 
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
