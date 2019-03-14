5ab7b0f0e84b41bcd0ffb09399bcf087b9ec65cd
denisyagupa <den_yagupa@mail.ru>
2019-03-14 14:00:39 +0300

    Denis
    SpashActivity

---
+++ Design/Sipliy/app/src/main/java/com/example/sipliy/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.sipliy;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e)
+        {
+
+        }
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainMenuActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5*1000);
+    }
+}
