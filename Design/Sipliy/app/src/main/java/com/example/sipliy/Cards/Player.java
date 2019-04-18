858961bb5b0b3b658447248013135a8209b04f5a
Averchenkov Nikita IKBO-02-17 <31746573+Averchenkov@users.noreply.github.com>
2019-04-18 11:46:31 +0300

    nikita

--- Design/Sipliy/app/src/main/java/com/example/sipliy/Cards/Player.java
+++ Design/Sipliy/app/src/main/java/com/example/sipliy/Cards/Player.java
@@ -13,10 +13,11 @@ public class Player
     private Items helmet;   //Головняк игрока
     private Items leftHand;     //Левая рука игрока
     private Items rightHand;    //Правая рука игока
-    private ArrayList<Items> otherClothes;  //Прочие шмотки игрока
-    private ArrayList<BuffOrDebuff> onetimeClothes;     //Разовые шмотки игока
+    private ArrayList<Object> otherClothes;  //Прочие шмотки игрока
+    private ArrayList<Object> onetimeClothes;     //Разовые шмотки игока
 
-    public Player(String name, boolean boolStatus, int level, int strength, int strengthClothes, Items shoes, Items armor, Items helmet, Items leftHand, Items rightHand)
+
+    public Player(String name, int level, int strength, int strengthClothes, Items shoes, Items armor, Items helmet, Items leftHand, Items rightHand)
     {
         this.name = name;
         this.level = level;
