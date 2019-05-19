d92c4f44b8716be3aa7a92ffe1c6c99b01c1b96b
denisyagupa <den_yagupa@mail.ru>
2019-05-19 21:09:46 +0300

    Doors

--- Design/Sipliy/app/src/main/java/com/example/sipliy/Cards/Doors.java
+++ Design/Sipliy/app/src/main/java/com/example/sipliy/Cards/Doors.java
@@ -22,6 +22,7 @@ public class Doors
 
     public void fill()
     {
+        //not_beating_with: 1 - рассовый признак, 2 - классовый признак, 3 - половой признак, 4 - уровневый признак
         doors.add(new Monster(20001, "Кальмадзилла", false, 18, 4, 2, 4, 4, 3, 0, 4, 0));
         doors.add(new Monster(20002, "3872 Орка", false, 10, 3, 1, 0, 0, 2, 0, 6, 0));
         doors.add(new Monster(20003, "Бульрог", false, 18, 5, 2, 4, 4, 0, 0, 0, 0));
@@ -32,6 +33,19 @@ public class Doors
         doors.add(new Monster(20008, "Наскипидаренные улитки", false, 4, 2, 1, 0, 0, 3, 0, 6, -2));
         doors.add(new Monster(20009, "Невыразимо Жуткий Неописуемый Ужас", false, 14, 4, 1, 0, 0, 0, 4, 4, 0));
         doors.add(new Monster(20010, "Король Тут", true, 16, 4, 2, 4, 3, 0, 0, 0, 0));
+        doors.add(new Monster(20011, "Сопливый Нос", false, 10, 3, 1, 0, 0, 0, 0, 0, -6));
+        doors.add(new Monster(20012, "Желатиновый Октаэдр", false, 2, 1, 1, 0, 0, 0, 0, 0, 1));
+        doors.add(new Monster(20013, "Газебо", false, 8, 2, 1, 0, 0, 0, 0, 0, 0));
+        doors.add(new Monster(20014, "Гарпистки", false, 4, 2, 1, 0, 0, 0, 3, 5, 0));
+        doors.add(new Monster(20015, "Лепрекон", false, 4, 2, 1, 0, 0, 3, 0, 5, 0));
+        doors.add(new Monster(20016, "Питбуль", false, 2, 1, 1, 0, 0, 0, 0, 0, 0));
+        doors.add(new Monster(20017, "Пасюк с кувалдой", false, 1, 1, 1, 0, 0, 0, 2, 3, 0));
+        doors.add(new Monster(20018, "Типа вошки", false, 1, 1, 1, 0, 0, 0, 0, 0, -6));
+        doors.add(new Monster(20019, "Адвокат", false, 6, 2, 1, 2, 5, 0, 0, 0, 0));
+        doors.add(new Monster(20020, "Гипогриф", false, 18, 4, 2, 4, 3, 0, 0, 0, 0));
+        doors.add(new Monster(20014, "Закос под вампиров", false, 12, 3, 1, 2, 2, 0, 0, 0, 0));
+        //Закончил писать монстров на 8 странице
+
         doors.add(new Races(24002, "Дварф"));
         doors.add(new Races(24003, "Эльф"));
         doors.add(new Races(24004, "Хафлинг"));
